50f77a32807232a2192f01251f9a68768593ad34
Faisal Jamil <faisaljamilcs@gmail.com>
2022-01-03 03:05:25 +0530

    fix: error msg

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -13,25 +13,28 @@ admin.initializeApp({
 
 async function decodeIDToken(req: Request, res: Response, next: NextFunction) {
   const header = req.headers?.authorization;
+  console.log('header:', header);
+
   if (
-    header !== 'Bearer null' &&
+    header !== 'Bearer' &&
     req.headers?.authorization?.startsWith('Bearer ')
   ) {
     const idToken = req.headers.authorization.split('Bearer ')[1];
     try {
       const decodedToken = await admin.auth().verifyIdToken(idToken);
-      // console.log(decodedToken);
+      console.log('decodedToken');
+      console.log(decodedToken);
       req.uid = decodedToken.uid;
       req.email = decodedToken.email || '';
       req.name = decodedToken.name || '';
       // console.log('VERIFIED');
     } catch (err) {
-      console.log(err.message);
+      console.log('Error:', err.message);
       return res.status(401).json({
         message: err.message,
       });
     }
-  } else if (header == undefined || header === 'Bearer null') {
+  } else if (header == undefined || header === 'Bearer') {
     return res.status(400).json({
       message: 'Token Missing',
     });
